afec32a41517f7bdbf538b920c01b0caaa838547
Script <lamp@ts.terrium.net>
2019-07-24 00:00:02 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 echo `date`
-wget -N -nv -i urls.txt
+wget -Ni urls.txt
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/fofo"
 git commit -am `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
