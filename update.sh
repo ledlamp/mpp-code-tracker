a5b1b3d584c965919d67a6510aeb0d776b76e268
Lamp <ledlamp89@gmail.com>
2020-04-16 17:28:43 -0400

    gafafafafafa

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 echo `date`
-wget -Ni urls.txt
+xargs -n 1 curl -sSO < urls.txt
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/fofo"
 git commit -am `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
