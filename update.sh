13cf7c9148bd2cebec6cb5a20dd5044a7dbe71ab
Lamp <ledlamp89@gmail.com>
2020-05-11 22:54:10 -0400

    FOLLOW REDIRECTS

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 echo `date`
-xargs -n 1 curl -sSO < urls.txt
+xargs -n 1 curl -sSOL < urls.txt
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/fofo"
 git commit -m `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
