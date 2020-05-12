7806e643397ed13476cdd928b466aa58212fb7b4
Lamp <ledlamp89@gmail.com>
2020-05-11 22:48:26 -0400

    gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    "fatal: Paths with -a does not make sense."

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ cd `dirname $0`
 echo `date`
 xargs -n 1 curl -sSO < urls.txt
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/fofo"
-git commit -am `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
+git commit -m `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
