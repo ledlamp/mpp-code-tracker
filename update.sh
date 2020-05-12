e56206eeeb092a3a5ad6f937beb40bdac284d3fc
Lamp <ledlamp89@gmail.com>
2020-05-11 23:15:43 -0400

    FML

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 cd `dirname $0`
 echo `date`
 xargs -n 1 curl -sSOL < urls.txt
+git add .
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/fofo"
 git commit -m `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
