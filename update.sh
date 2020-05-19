e6c4d5ab610b2d0c0cccf62439e4f2b7289014fd
Lamp <ledlamp89@gmail.com>
2020-05-19 01:30:01 -0400

    I think this will fix commit command

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ echo `date`
 xargs -n 1 curl -sSOL < urls.txt
 git add .
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/fofo"
-git commit -m `git diff --name-only | tr '\n' ' '` --author="Script <lamp@ts.terrium.net>" && git push -u origin master
+git commit -m "`git diff --name-only | tr '\n' ' '`" --author="Script <lamp@ts.terrium.net>" && git push -u origin master
