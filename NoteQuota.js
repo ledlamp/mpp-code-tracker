1c6697c8bffe9120a14d03e6ba1ef7bc72dafdc6
Script <lamp@ts.terrium.net>
2021-02-20 00:00:05 -0500

    Changes detected

--- NoteQuota.js
+++ NoteQuota.js
@@ -1,85 +1,25 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Multiplayer Piano</title>
+    <meta name="description"
+          content="An online piano you can play alone or with others in real-time. MIDI support, 88 keys, velocity sensitive.  You can show off your skill or chat while listening to others play.">
 
-var exports = (function() {
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-882009-7"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-	var NoteQuota = function(cb) {
-		this.cb = cb;
-		this.setParams();
-		this.resetPoints();
-	};
+      gtag('config', 'UA-882009-7');
+    </script>
 
-	NoteQuota.PARAMS_LOBBY = {allowance: 200, max: 600};
-	NoteQuota.PARAMS_NORMAL = {allowance: 400, max: 1200};
-	NoteQuota.PARAMS_RIDICULOUS = {allowance: 600, max: 1800};
-	NoteQuota.PARAMS_OFFLINE = {allowance: 8000, max: 24000, maxHistLen: 3};
-
-	NoteQuota.prototype.getParams = function() {
-		return {
-			m: "nq",
-			allowance: this.allowance,
-			max: this.max,
-			maxHistLen: this.maxHistLen
-		};
-	};
-
-	NoteQuota.prototype.setParams = function(params) {
-		params = params || NoteQuota.PARAMS_OFFLINE;
-		var allowance = params.allowance || this.allowance || NoteQuota.PARAMS_OFFLINE.allowance;
-		var max = params.max || this.max || NoteQuota.PARAMS_OFFLINE.max;
-		var maxHistLen = params.maxHistLen || this.maxHistLen || NoteQuota.PARAMS_OFFLINE.maxHistLen;
-		if(allowance !== this.allowance || max !== this.max || maxHistLen !== this.maxHistLen) {
-			this.allowance = allowance;
-			this.max = max;
-			this.maxHistLen = maxHistLen;
-			this.resetPoints();
-			return true;
-		}
-		return false;
-	};
-
-	NoteQuota.prototype.resetPoints = function() {
-		this.points = this.max;
-		this.history = [];
-		for(var i = 0; i < this.maxHistLen; i++)
-			this.history.unshift(this.points);
-		if(this.cb) this.cb(this.points);
-	};
-
-	NoteQuota.prototype.tick = function() {
-		// keep a brief history
-		this.history.unshift(this.points);
-		this.history.length = this.maxHistLen;
-		// hook a brother up with some more quota
-		if(this.points < this.max) {
-			this.points += this.allowance;
-			if(this.points > this.max) this.points = this.max;
-			// fire callback
-			if(this.cb) this.cb(this.points);
-		}
-	};
-
-	NoteQuota.prototype.spend = function(needed) {
-		// check whether aggressive limitation is needed
-		var sum = 0;
-		for(var i in this.history) {
-			sum += this.history[i];
-		}
-		if(sum <= 0) needed *= this.allowance;
-		// can they afford it?  spend
-		if(this.points < needed) {
-			return false;
-		} else {
-			this.points -= needed;
-			if(this.cb) this.cb(this.points); // fire callback
-			return true;
-		}
-	};
-
-	return NoteQuota;
-
-})();
-
-if(typeof module !== "undefined") {
-	module.exports = exports;
-} else {
-	this.NoteQuota = exports;
-}
+</head>
+<body style="
+    margin: 0;
+">
+<iframe src="https://multiplayerpiano.net/" style="width: 100%;border: 0;height: 100%;">
+</iframe>
+</body>
+</html>
