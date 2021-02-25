8c456a35b3b45be1ceacd7fee6e81e446c240225
Script <lamp@ts.terrium.net>
2021-02-25 00:00:05 -0500

    Changes detected

--- workerTimer.js
+++ workerTimer.js
@@ -1,28 +1,6 @@
-<html>
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>Multiplayer Piano</title>
-    <meta name="description"
-          content="An online piano you can play alone or with others in real-time. MIDI support, 88 keys, velocity sensitive.  You can show off your skill or chat while listening to others play.">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-882009-7"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-882009-7');
-    </script>
-
-</head>
-<body style="
-    margin: 0;
-">
-
-    <script>
-      document.body.innerHTML = document.body.innerHTML + "<iframe src=\"https://mppclone.com/\" style=\"width: 100%;border: 0;height: 100%;\">";
-    </script>
-</iframe>
-</body>
-</html>
+self.onmessage = function(event)
+	{
+		setTimeout(function(){
+			postMessage({args:event.data.args});
+		},event.data.delay);
+	}
